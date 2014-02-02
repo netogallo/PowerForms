33477c416ef78a2e313a48f0851d87c311236b45
Ernesto Rodriguez <neto@netowork.me>
2014-02-02 01:25:39 +0100

    begin filtering

--- routes/frontend.js
+++ routes/frontend.js
@@ -1,35 +1,77 @@
 exports.init = function(models){
 
-	var objs={};
+    var objs={};
 
-	objs.top = function(req,res){
+    objs.top = function(req,res){
 
-	    res.render('homepage');
-	}
+	res.render('homepage');
+    };
 
-	objs.editor = function(req,res){
+    objs.editor = function(req,res){
 
-	    res.render('testing');
-	}
+	res.render('testing');
+    };
 
-	objs.formname = function(req,res){
-		models.Letter.find({name:req.params.name}, function(err, letter){
-			var content = {letter: {name:letter[0].name, displayname:letter[0].displayname, letter:letter[0].letter}};
-			if(letter.length>=1) {
-				letter[0].getTags(function(err,arg) {
-					console.log(arg);
-					content.letter.tags=arg;
-					res.render('letter',content);
-				});
-			} else {
-				console.log("error");
-			}
+    objs.formname = function(req,res){
+	models.Letter.find({name:req.params.name}, function(err, letter){
+	    var content = {letter: {name:letter[0].name, displayname:letter[0].displayname, letter:letter[0].letter}};
+	    if(letter.length>=1) {
+		letter[0].getTags(function(err,arg) {
+		    console.log(arg);
+		    content.letter.tags=arg;
+		    res.render('letter',content);
 		});
-	}
+	    } else {
+		console.log("error");
+	    }
+	});
+    };
 
-	objs.createform = function(req,res){
-		res.render('create');
-	}
+    objs.createform = function(req,res){
+	res.render('create');
+    };
 
-	return objs;
+    objs.searchTags = function(req, res){
+
+	if(req.body.tags 
+	   && req.body.tags.length){
+
+	    var tags = req.body.tags;
+	    
+	    var q = [];
+
+	    for(var i=0; i < tags.length; i++){
+
+		q.push(req.body.tags[i]);
+	    }
+
+	models.Tag.find(
+	    {name: q},
+	    function(err, tags){
+		
+		console.log(tags);
+		
+		// var letters = function(rec,tags,letters){
+
+		//     if(tags.length < 1){
+
+		// 	res.json(letters);
+		//     }else{
+
+		// 	var t = tags.pop();
+		// 	t.getLetters(function(letters){
+		// 	    rec(rec,tags,
+		//     }
+		// }
+
+		res.json(tags[0].letters);
+
+	    });
+	}else{
+
+	    res.json({error: 'No query given'});
+	}
+    };
+
+    return objs;
 };
--- routes/models.js
+++ routes/models.js
@@ -5,18 +5,16 @@ exports.models = function(err, db){
         last      : String,
         fbid      : String,
     }, {
-        methods: {
-            fullName: function () {
-                return this.name + ' ' + this.surname;
-            }
-        },
-        validations: {
+        methods: {},
+	validations: {
+	    fbid: orm.validators.unique('User name already taken')
         }
     });
 
     exports.Tag = db.define("tag", {
         name      : String        
     }, {
+	autoFetchLimit: 3,
         methods: {
             fullName: function () {
                 return this.name + ' ';
@@ -38,53 +36,18 @@ exports.models = function(err, db){
             }
         },
         validations: {
+
+	    name: orm.validators.unique('name must be unique')
         }
     });
 
-    exports.Letter.hasMany('tags',exports.Tag);
+    //exports.Tag.hasMany('letter',exports.Letter);
+    exports.Letter.hasMany('tags',exports.Tag,
+			   {},
+			   {
+			       reverse: 'letters',
+			       autoFetch: true
+			   });
 
-    db.sync();
-
-    var tag1 = new exports.Tag({
-        name:"yellow",
-    });
-
-    tag1.save(function(arg) {
-        
-        var tag2 = new exports.Tag({
-            name:"green",
-        });
-
-        tag2.save(function(arg) {
-
-        var contractcancelation = new exports.Letter({
-            name:"O2ContractCancelation",
-            displayname: "O2 Contract Cancelation",
-            letter:"Max Mustermann, Straße/Hausnummer, PLZ/Ort<br>" +
-                    "O2 D2 GmbH<br>" +
-                    "O2-Kundenbetreuung<br>" +
-                    "40875 Ratingen<br><br><br>" +
-                    "Kündigung des Handyvertrags mit der Rufnummer:<br>" +
-                    "Kundennummer:<br><br>" +
-
-                    "Sehr geehrte Damen und Herren,<br><br>" +
-
-                    "hiermit möchte ich meinen Vertrag fristgerecht zum nächstmöglichen Zeitpunkt kündigen. Bitte senden Sie mir eine schriftliche Bestätigung der Kündigung unter Angabe des Beendigungszeitpunktes zu.<br><br>" +
-
-                    "Vielen Dank.<br><br>" +
-
-                    "Mit freundlichen Grüßen<br><br>" +
-
- 
-
-                    "Vorname Name",
-            html:"#",
-            tags: [tag1,tag2],      
-        });
-
-        contractcancelation.save();
-    })});
-
-
-    
+    db.sync();    
 }
